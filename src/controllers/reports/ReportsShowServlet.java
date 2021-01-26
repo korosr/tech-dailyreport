9ff989b5aee146422c3d4a5756fdf46011b4977c
korosr <kaoriisa1206@gmail.com>
2021-01-26 16:58:59 +0900

    日報機能追加

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -1,7 +1,8 @@
-package controllers.toppage;
+package controllers.reports;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,17 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Report;
+import util.DBUtil;
+
 /**
- * Servlet implementation class TopPageIndexServlet
+ * Servlet implementation class ReportsShowServlet
  */
-@WebServlet(name = "index.html", urlPatterns = { "/index.html" })
-public class TopPageIndexServlet extends HttpServlet {
+@WebServlet(name = "reports/show", urlPatterns = { "/reports/show" })
+public class ReportsShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TopPageIndexServlet() {
+    public ReportsShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,11 +32,17 @@ public class TopPageIndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(request.getSession().getAttribute("flush") != null) {
-	        request.setAttribute("flush", request.getSession().getAttribute("flush"));
-	        request.getSession().removeAttribute("flush");
-	    }
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
+		EntityManager em = DBUtil.createEntityManager();
+
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("report", r);
+        request.setAttribute("_token", request.getSession().getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
         rd.forward(request, response);
 	}
+
 }
