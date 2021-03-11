3a82dac67a3a1440ab32b577a7f8659c1a961224
korosr <kaoriisa1206@gmail.com>
2021-03-11 17:03:50 +0900

    いいね機能追加

---
+++ src/controllers/reports/ReportsReactionServlet.java
@@ -0,0 +1,59 @@
+package controllers.reports;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Reaction;
+import util.DBUtil;
+
+/**
+ * Servlet implementation class ReportsReactionServlet
+ */
+@WebServlet(name = "reports/reaction", urlPatterns = { "/reports/reaction" })
+public class ReportsReactionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsReactionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		EntityManager em = DBUtil.createEntityManager();
+
+		//レポートID取得
+		int reportId = Integer.parseInt(request.getParameter("id"));
+
+		//ログインユーザ取得
+		Employee e = (Employee) request.getSession().getAttribute("login_employee");
+		int loginUserId = e.getId();
+
+		System.out.println("reportId" + reportId);
+		System.out.println("loginUserId" + loginUserId);
+
+		Reaction r = new Reaction();
+		r.setEmployee_id(loginUserId);
+		r.setReport_id(reportId);
+
+		em.getTransaction().begin();
+		em.persist(r);
+		em.getTransaction().commit();
+
+		em.close();
+        response.sendRedirect(request.getContextPath() + "/reports/show");
+	}
+}
---
+++ src/models/Reaction.java
@@ -0,0 +1,47 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "reactions")
+@Entity
+public class Reaction {
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+	@Column(name = "employee_id", nullable = false)
+	private Integer employee_id;
+
+	@Column(name = "report_id", nullable = false)
+	private Integer report_id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(Integer employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public Integer getReport_id() {
+		return report_id;
+	}
+
+	public void setReport_id(Integer report_id) {
+		this.report_id = report_id;
+	}
+}
